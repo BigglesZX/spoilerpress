d3831e517ddd63f8ac87fd427e8c074bc154d715
James Tiplady <biggleszx@biggleszx.com>
2015-03-23 12:04:09 +0000

    Infinitely quicker, searching by dict word instead of the other way round. Almost there.

--- main.py
+++ main.py
@@ -6,35 +6,24 @@ WORDLIST = 'dict/en.txt'
 
 
 def main():
-    start_w = time.clock()
     wordlist = set(line.strip() for line in open(WORDLIST))
     board_lines = ['PITBU', 'WZRSV', 'OIRNO', 'BFAEN', 'PLSXA',]
     board_lines = [list(l) for l in board_lines]
     board_letters = list(chain.from_iterable(board_lines))
-    end_w = time.clock()
-    duration_w = (end_w - start_w) / 1000
+    board_letters = list(''.join(board_letters).lower())
+    print "Testing letters: {0}".format(''.join(board_letters).upper())
 
     found_words = []
-    count = 0
-    start_s = time.clock()
-    for i in range(0, len(board_letters)+1):
-        for subset in permutations(board_letters, i):
-            if subset in wordlist:
-                found_words.append(subset)
-            count += 1
+    for word in wordlist:
+        if all([l in board_letters for l in list(word)]):
+            # FIXME: this doesn't take into account multiple uses of same letter
+            found_words.append(word)
 
-            if count % 1000000 == 0:
-                print "{0} combinations tried".format(count)
-
-    end_s = time.clock()
-    duration_s = (end_s - start_s) / 1000
-    print "{0} words found".format(len(count))
-    print "wordlist load took {0}s".format(duration_w)
-    print "search took {0}s".format(duration_s)
+    found_words.sort(key=len, reverse=True)
+    print "{0} words found".format(len(found_words))
+    for word in found_words[:10]:
+        print word
 
 
 if __name__ == '__main__':
     main()
-
-
-# http://stackoverflow.com/questions/746082/how-to-find-list-of-possible-words-from-a-letter-matrix-boggle-solver
